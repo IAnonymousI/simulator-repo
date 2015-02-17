fd18d5fb895601281381d63c5b78eb7a2526c3d1
IAnonymousI <18jinyoung.park@boiseschools.net>
2015-02-16 20:40:02 -0700

    One class added to the Prototype package.(Physics)

---
+++ Prototypes/src/Physics.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+public class Physics extends JPanel implements ActionListener, KeyListener{
+	
+	//Components
+	Timer time = new Timer(5, this);
+	double x = 250, y = 250, velX = 0, velY = 0, accX = 1, accY = 1;
+	
+	public Physics(){
+		time.start();
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		Graphics2D g2 = (Graphics2D) g;
+		Ellipse2D circle = new Ellipse2D.Double(x, y, 10, 10);
+		g2.fill(circle);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		repaint();
+		
+		x += velX;
+		y += velY;
+	}
+	public void up(){
+		velX = 0;
+		velY = -1;
+	}
+	public void down(){
+		velX = 0;
+		velY = 1;
+	}
+	public void left(){
+		velX = -1;
+		velY = 0;
+	}
+	public void right(){
+		velX = 1;
+		velY = 0;
+	}
+	
+	public void keyPressed(KeyEvent e){
+		int code = e.getKeyCode();
+		if(code == KeyEvent.VK_UP){
+			up();
+		}
+		else if(code == KeyEvent.VK_DOWN){
+			down();
+		}
+		else if(code == KeyEvent.VK_LEFT){
+			left();
+		}
+		else if(code == KeyEvent.VK_RIGHT){
+			right();
+		}
+	}
+	public void keyTyped(KeyEvent e){}
+	public void keyReleased(KeyEvent e){
+		velX = 0;
+		velY = 0;
+	}
+}
--- Prototypes/src/Run.java
+++ Prototypes/src/Run.java
@@ -3,6 +3,13 @@ import java.awt.*;
 
 public class Run{
 	public static void main(String[] args){
-		new StatusWindow();
+		JFrame frame = new JFrame("Prototype_3");
+		Physics p = new Physics();
+		p.setBackground(Color.BLACK);
+		frame.add(p);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(600, 600);
+		frame.getContentPane().setBackground(Color.BLACK);
 	}
 }
