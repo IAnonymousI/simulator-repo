5360b8ddbc3dbe726f69e50d9d2b688487f5932d
IAnonymousI <18jinyoung.park@boiseschools.net>
2015-03-17 19:21:27 -0600

    Second Status window added.

--- Prototypes/src/Run.java
+++ Prototypes/src/Run.java
@@ -3,14 +3,6 @@ import java.awt.*;
 
 public class Run{
 	public static void main(String[] args){
-		JFrame frame = new JFrame("Prototype_3");
-		Canvas p = new Canvas();
-		StatusWindow2 status = new StatusWindow2();
-		p.setBackground(Color.BLACK);
-		frame.add(p);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setSize(600, 600);
-		frame.getContentPane().setBackground(Color.BLACK);
+		new StatusWindow2();
 	}
 }
--- Prototypes/src/StatusWindow2.java
+++ Prototypes/src/StatusWindow2.java
@@ -1,33 +1,53 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class StatusWindow2 extends JFrame implements ActionListener{
 	
-	private JButton apply = new JButton("Apply");
+	//Components
+	private final JLabel[] label = {new JLabel("Red Ball"), new JLabel("Blue Ball"), new JLabel("White Ball")};
+	private final String[] status = {"Position", "Velocity", "Acceleration"};
+	private final JLabel[] statusLabel = new JLabel[3];
+	private final JButton applyButton = new JButton("Apply");
 	
 	public StatusWindow2(){
-		super("StatusWindow");
 		
-		//Default Window Settings
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(500, 500);
-		setVisible(true);
+		super("Status Window");
+		setLayout(new GridLayout(5, 5, 10, 10));
 		
-		
-		add(apply);
-	}
-	
-	public final void setDesign(){
-		try{
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch(Exception e){
-			
+		for(int i = 0; i <= 2; i++){
+			statusLabel[i] = new JLabel(status[i]);
 		}
+		
+		add(new JLabel());
+		
+		for(int i = 0; i <= 2; i++){
+			add(label[i]);
+		}
+		
+		for(int i = 0; i <= 2; i++){
+			add(statusLabel[i]);
+			add(new JTextField());
+			add(new JTextField());
+			add(new JTextField());
+		}
+		
+		for(int i = 0; i < 3; i++){
+			add(new JLabel());
+		}
+		add(applyButton);
+		
+		applyButton.addActionListener((ActionListener) this);
+		
+		setVisible(true);
+		setSize(400, 200);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-	@Override
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent event){
+		
 	}
-} 
+}
